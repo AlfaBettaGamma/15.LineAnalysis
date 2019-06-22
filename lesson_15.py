9c31784b358bfa6b9d7af5bcf02e5a2aa086d3c7
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-06-22 12:23:32 +0300

    update

--- lesson_15.py
+++ lesson_15.py
@@ -6,12 +6,12 @@ def LineAnalysis(line):
   for  i in range(len(l)):
     if l[i] == '*':
       sort.append(i)
-  if len(sort) == 2:
+  if len(sort) <= 2:
     b = True
     return b
   for i in range(len(sort)):
-    while j is not (len(sort) - 1):
-      if sort[j] * 2 == sort[j + 1]:
+    while j is not (len(sort)-1):
+      if sort[j]*2 == sort[j + 1]:
         b = True
       else:
         b = False
